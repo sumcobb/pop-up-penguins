d38ff99aa44ee7981e16876edd135145aadae825
turner36 <summerkt99@gmail.com>
2017-11-25 19:05:15 -0800

    initial files from following instructions

---
+++ penguins.js
@@ -0,0 +1,14 @@
+/* Summer Turner
+* CSCI 342: Web Scripting, Fall 2017
+*/
+"use strict";
+
+$(document).ready( function() {
+
+    //This code will run after your page loads
+
+    $(".yeti").mousedown(function() {
+        alert("Yaaaarrrr!");
+    });
+
+});
